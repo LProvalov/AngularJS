4c45373e9701150c0b0744afdee6709d24a3b0e7
lev.provalov-home <lev.provalov@gmail.com>
2017-09-19 20:51:42 +0300

    Changes in BaseDAO

--- Angular_CLI/ksen-port-serv/src/DAO/BaseDAO.ts
+++ Angular_CLI/ksen-port-serv/src/DAO/BaseDAO.ts
@@ -3,7 +3,7 @@ import { RepositoryBase } from './../providers/repositoryBase';
 
 var Schema = mongoose.Schema;
 
-abstract class BaseDAO<D extends mongoose.Document> {
+export abstract class BaseDAO<D extends mongoose.Document> {
     private _document: D;
     private _collectionName: string;
     private _model: mongoose.Model<D>;
@@ -18,7 +18,43 @@ abstract class BaseDAO<D extends mongoose.Document> {
         this._model = mongoose.model<D>(modelName, mongooseSchema, collectionName, true);
         this._repository = new RepositoryBase<D>(this._model);
         Object.seal(this._repository);
+        this.readById = this.readById.bind(this);
+        this.create = this.create.bind(this);
     }
 
+    readById(id: string): Promise<D> {
+        return new Promise<D>( (resolve, reject) => {
+            this._repository.findById(id, (err, res) => {
+                if (err) reject(err);
+                resolve(res);
+            });
+        });
+    }
 
+    create(obj: D): Promise<D>{
+        return new Promise<D>( (resolve, reject) => {
+            this._repository.create(obj, (err, res) => {
+                if(err) reject(err);
+                resolve(res);
+            });
+        });
+    }
+
+    update(obj: D): Promise<boolean> {
+        return new Promise<boolean>( (resolve, reject) => {
+            this._repository.update(obj._id, obj, (err, res) => {
+                if(err) reject(err);
+                resolve(true);
+            });
+        });
+    }
+
+    delete(obj: D): Promise<boolean> {
+        return new Promise<boolean>( (resolve, reject) => {
+            this._repository.delete(obj.id, (err, res) => {
+                if(err) reject(err);
+                resolve(true);
+            });
+        });
+    }
 }
